7a1f38f8313608e729bb166f5200654a9602015a
Daniel Osorio <josorio@personalsoft.com.co>
2020-07-06 18:41:21 -0500

    update all Https methods

--- src/main/java/com/testpersonal/TestService/controller/Controller.java
+++ src/main/java/com/testpersonal/TestService/controller/Controller.java
@@ -1,18 +1,27 @@
 package com.testpersonal.TestService.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.testpersonal.TestService.model.dto.ErrorDto;
 import com.testpersonal.TestService.model.dto.UserDto;
 
 @RestController
 public class Controller {
+	
+	@Value("${properties.myvalue}")
+	String value;
 	
 	@GetMapping("/{path}")
 	public ResponseEntity<String> getWithParams(
@@ -24,21 +33,25 @@ public class Controller {
 	
 	@GetMapping
 	public ResponseEntity<UserDto> get() {
+		System.out.println(value);
 		return ResponseEntity.ok(new UserDto("daniel", "osorio"));
 	}
 	
 	@PostMapping
-	public String create() {
-		return "post working";
+	public ResponseEntity<Object> create(
+			@Valid @RequestBody UserDto user) {
+		if(user.getName().length()>15) return ResponseEntity.ok(new ErrorDto("Name length max exceeded","400"));
+		return ResponseEntity.ok(user);
 	}
 	
-	@PutMapping("")
-	public String update() {
-		return "put working";
+	@PutMapping
+	@ResponseBody
+	public UserDto update() {
+		return new UserDto("Kevin", "Arias3");
 	}
-
+	
 	@DeleteMapping
 	public String delete() {
-		return "delete working";
+		return "Delete working";
 	}
 }
--- src/main/java/com/testpersonal/TestService/model/dto/UserDto.java
+++ src/main/java/com/testpersonal/TestService/model/dto/UserDto.java
@@ -1,24 +1,38 @@
 package com.testpersonal.TestService.model.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UserDto {
 	
+	@NotBlank(message = "Name is required.")
 	private String name;
+	
+	@NotNull(message = "LastName is required.")
 	private String lastName;
 	
+	@NotBlank(message = "DirectionOfHouse is required.")
+	private String directionOfHouse;
+	
 	public UserDto() {}
-		
+	
 	public UserDto(String name, String lastName) {
 		super();
 		this.name = name;
 		this.lastName = lastName;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
@@ -26,5 +40,12 @@ public class UserDto {
 		this.lastName = lastName;
 	}
 
+	public String getDirectionOfHouse() {
+		return directionOfHouse;
+	}
+
+	public void setDirectionOfHouse(String directionOfHouse) {
+		this.directionOfHouse = directionOfHouse;
+	}
 
 }
